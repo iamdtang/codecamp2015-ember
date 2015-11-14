2401fada2295d2a14ba345d05b8104457631a88c
David Tang <dtang859@gmail.com>
2015-11-14 00:34:05 -0800

    update

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,7 +1,7 @@
 import DS from 'ember-data';
 
 export default DS.RESTAdapter.extend({
-  host: 'http://itp-api.herokuapp.com',
-  // host: 'http://localhost:3000',
+  // host: 'http://itp-api.herokuapp.com',
+  host: 'http://localhost:3000',
   namespace: 'api/v2'
 });
--- app/adapters/artist.js
+++ app/adapters/artist.js
@@ -1,5 +1,5 @@
 import ApplicationAdapter from './application';
 
 export default ApplicationAdapter.extend({
-  namespace: 'api/v2'
+  // namespace: 'api/v1'
 });
